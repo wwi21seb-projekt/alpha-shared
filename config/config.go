3b8c8d8ff29670c1b070c331ea6d35656a8176bb
Frecherenkel60 <rieger.kevin01@gmail.com>
2024-05-29 14:43:24 +0200

    feat: remove user id in favor of username

--- config/config.go
+++ config/config.go
@@ -7,29 +7,33 @@ import (
 
 // AlphaConfig holds all possible configuration values
 type AlphaConfig struct {
-	PostgresHost     string
-	PostgresPort     string
-	PostgresDB       string
-	PostgresUser     string
-	PostgresPassword string
-	SchemaName       string
-	Port             string
-	UserServiceURL   string
-	PostServiceURL   string
+	PostgresHost           string
+	PostgresPort           string
+	PostgresDB             string
+	PostgresUser           string
+	PostgresPassword       string
+	SchemaName             string
+	Port                   string
+	UserServiceURL         string
+	PostServiceURL         string
+	MailServiceURL         string
+	NotificationServiceURL string
 }
 
 // LoadConfig loads environment variables into an AlphaConfig struct
 func LoadConfig() (*AlphaConfig, error) {
 	config := &AlphaConfig{
-		PostgresHost:     getEnv("POSTGRES_HOST", "localhost"),
-		PostgresPort:     getEnv("POSTGRES_PORT", "5432"),
-		PostgresDB:       getEnv("POSTGRES_DB", "mydatabase"),
-		PostgresUser:     getEnv("POSTGRES_USER", "myuser"),
-		PostgresPassword: getEnv("POSTGRES_PASSWORD", "mypassword"),
-		SchemaName:       getEnv("SCHEMA_NAME", "public"),
-		Port:             getEnv("PORT", "8080"),
-		UserServiceURL:   getEnv("USER_SERVICE_URL", "http://localhost:50051"),
-		PostServiceURL:   getEnv("POST_SERVICE_URL", "http://localhost:50052"),
+		PostgresHost:           getEnv("POSTGRES_HOST", "localhost"),
+		PostgresPort:           getEnv("POSTGRES_PORT", "5432"),
+		PostgresDB:             getEnv("POSTGRES_DB", "mydatabase"),
+		PostgresUser:           getEnv("POSTGRES_USER", "myuser"),
+		PostgresPassword:       getEnv("POSTGRES_PASSWORD", "mypassword"),
+		SchemaName:             getEnv("SCHEMA_NAME", "public"),
+		Port:                   getEnv("PORT", "8080"),
+		UserServiceURL:         getEnv("USER_SERVICE_URL", "http://localhost:50051"),
+		PostServiceURL:         getEnv("POST_SERVICE_URL", "http://localhost:50052"),
+		MailServiceURL:         getEnv("MAIL_SERVICE_URL", "http://localhost:50053"),
+		NotificationServiceURL: getEnv("NOTIFICATION_SERVICE_URL", "http://localhost:50054"),
 	}
 
 	// Add any necessary validation here
